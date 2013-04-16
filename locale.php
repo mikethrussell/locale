cb2cf667f67a3f86d62d209cf822da6119ca4b18
Mike Thrussell <mike.thrussell@googlemail.com>
2013-04-16 22:13:26 +0100

    sqlite

--- locale.php
+++ locale.php
@@ -1,20 +1,23 @@
 #!/usr/bin/php
 
 <?php
-//notes to self - need some way (text file?) of registering 'home' or 'away' to know if to run the script or not. away for how long?/.. hmmm 
-
 
 //credit: http://www.nmcmahon.co.uk/getting-the-distance-between-two-locations-using-google-maps-api-and-php
 
 
 //enter your user ID. Obtain from: https://latitude.google.com/latitude/b/0/apps
 
-$mike_lat = '7649347323393564615';
-$caroline_lat = '7405538217839576382';
+$mike_lat = 'XXXXXXXXXXXXXXXXXXXX';
+$caroline_lat = 'XXXXXXXXXXXXXXXXXXXX';
 
 //enter home address eg. 'LS14 6HG' or '24 Green Tree Avenue, York' etc.
 
-$home = 'HG3 2DS';
+$home = 'PO5T C0D3';
+
+
+//STATES: 0 = 'home', 1 = 'arriving', 2 = 'away', 3 = 'holiday'
+
+
 
 //FUNCTIONS
 
@@ -73,57 +76,66 @@ return array($lat,$lon);
 //FUNCTIONS END
 
 
-//setup sqlite db
+//setting things up
+date_default_timezone_set('GMT');
+$timestamp = time();
+$states = array();
+
+//SETUP SQLITE DB
 
 date_default_timezone_set('GMT');
 
 try {
-    /**************************************
-    * Create databases and                *
-    * open connections                    *
-    **************************************/
- 
-    // Create (connect to) SQLite database in file
+    
+    // Create or connect to SQLite database in file
     $file_db = new PDO('sqlite:states');
     // Set errormode to exceptions
     $file_db->setAttribute(PDO::ATTR_ERRMODE, 
                             PDO::ERRMODE_EXCEPTION);
  
-    /**************************************
-    * Create tables                       *
-    **************************************/
- 
-    // Create table messages
-    $file_db->exec("CREATE TABLE IF NOT EXISTS states (
+
+//Create states table if not exists
+$file_db->exec("CREATE TABLE IF NOT EXISTS states (
                     id INTEGER PRIMARY KEY, 
-                    user TEXT, 
-                    state TEXT, 
+                    user VARCHAR UNIQUE, 
+                    state VARCHAR, 
                     time DATETIME)");
- 
- 
-    /**************************************
-    * Set sqlite data                    *
-    **************************************/
- 
-    // Array with some test data to insert to database             
-    $states = array(
-                  array('user' => 'Hello!',
-                        'state' => 'Just testing...',
-                        'time' => 1327301464),
-                  array('user' => 'Hello again!',
-                        'state' => 'More testing...',
-                        'time' => 1339428612),
-                  array('user' => 'Hi!',
-                        'state' => 'SQLite3 is cool...',
-                        'time' => 1327214268)
-                );
- 
+                    
+//set inital data (if none)
+$initial_states = array (
+array('user' => 'mike',
+                        'state' => 0,
+                        'time' => $timestamp),
+array('user' => 'caroline',
+                        'state' => 0,
+                        'time' => $timestamp),
+array('user' => 'home',
+                        'state' => 0,
+                        'time' => $timestamp)
+);     
 
- 
+ 	$insert = "INSERT OR IGNORE INTO states (user, state, time) 
+                VALUES (:user, :state, :time)";
+                
+	$stmt = $file_db->prepare($insert);
 
+// Bind parameters to statement variables
+    $stmt->bindParam(':user', $user);
+    $stmt->bindParam(':state', $state);
+    $stmt->bindParam(':time', $timestamp);
+ 
+    // Loop thru all messages and execute prepared insert statement
+    foreach ($initial_states as $is) {
+      // Set values to bound variables
+      $user = $is['user'];
+      $state = $is['state'];
+      $timestamp = $is['time'];
+ 
+      // Execute statement
+      $stmt->execute();
+    }
 
- 
- 
+//END SETUP SQLITE DB
 
 
 //convert everything to lon lat
@@ -132,58 +144,91 @@ $caroline_loc = getLatitude($caroline_lat);
 $home_loc = getLatLong($home);
 
 
-//STATES = 0 = 'home', 1 = 'away', 2 = 'holiday'
+//get $c_prev_state
+
+$stmt = $file_db->query('SELECT state FROM states WHERE user IS "caroline"');
+$c_prev_state = $stmt->fetchColumn(0);
+
+//get $m_prev_state
+
+$stmt = $file_db->query('SELECT state FROM states WHERE user IS "mike"');
+$m_prev_state = $stmt->fetchColumn(0);
+
+//get $house_state
+
+$stmt = $file_db->query('SELECT state FROM states WHERE user IS "home"');
+$house_state = $stmt->fetchColumn(0);
+
+
 
 //set caroline state
 
-//get $c_prev_state
-
 $distance = Haversine($caroline_loc, $home_loc);
 
-if ($distance <= 0.5) {$c_state = 1;}
-//arriving
+if ($distance <= 0.5) {$c_state = 0;}
+elseif ($distance >0.5 && $distance <= && $c_prev_state != 0) {$c_state = 1;}
 elseif ($distance >0.5 && $distance <= 100) {$c_state = 2;}
 else {$c_state = 3;}
+
+$state =  array('user' => 'caroline',
+                        'state' => $c_state,
+                        'time' => $timestamp );
+array_push($states, $state); 
+
 
 
 //set mike state
 
-//get $m_prev_state
-
 $distance = Haversine($mike_loc, $home_loc);
 
-if ($distance <= 0.5) {$m_state = 1;}
-//arriving
+if ($distance <= 0.5) {$m_state = 0;}
+elseif ($distance >0.5 && $distance <= && $m_prev_state != 0) {$m_state = 1;}
 elseif ($distance >0.5 && $distance <= 100) {$m_state = 2;}
 else {$m_state = 3;}
 
+$state =  array('user' => 'mike',
+                        'state' => $m_state,
+                        'time' => $timestamp );
+array_push($states, $state); 
 
-//get house state
 
 
-if ($c_state == 1 || $m_state == 1 && $house_state != 'home') {
+if ($c_state == 0 || $m_state == 0 && $house_state != 0) {
 
 //do home functions
 
+// ***INSERT HOME STATE FUNCTIONS HERE***
+
+/* e.g. (for cURL code)
+
 $ch = curl_init("http://192.168.1.42:8080/json.htm?type=command&param=switchscene&idx=1&switchcmd=Off");
 curl_exec($ch);
-$ch = curl_init("http://192.168.1.42:8080/json.htm?type=command&param=switchscene&idx=1&switchcmd=On");
-curl_exec($ch);
-
 curl_close($ch);
 
+*/
+
 //set house state to 'home'
+$state =  array('user' => 'home',
+                        'state' => 0,
+                        'time' => $timestamp );
+                        
+array_push($states, $state); 
 
 }
 
 
-if ($m_state == 'arriving' && $m_prev_state != 'arriving') {
+if ($m_state == 1 && $m_prev_state != 1) {
 // do mike arriving functions
+
+// ***INSERT MIKE ARRIVING FUNCTIONS HERE***
 
 }
 
-if ($c_state == 'arriving' && $c_prev_state != 'arriving') {
-// do mike arriving functions
+if ($c_state == 1 && $c_prev_state != 1) {
+// do caroline arriving functions
+
+// ***INSERT CAROLINE ARRIVING FUNCTIONS HERE***
+
 
 }
 
@@ -191,49 +236,75 @@ if ($c_state == 'arriving' && $c_prev_state != 'arriving') {
 if ($m_state == 3 && $c_state == 3) {
 
 //do holiday functions
+
+// ***INSERT HOLIDAY STATE FUNCTIONS HERE***
+
 //set house state to 'holiday'
+$state =  array('user' => 'home',
+                        'state' => 3,
+                        'time' => $timestamp );
+                        
+array_push($states, $state); 
 
 }
 
-elseif ($m_state >=2 && $c_state >=2 && $house_state != 'away'){
+elseif ($m_state >=2 && $c_state >=2 && $house_state != 2){
 
 //do away functions
+
+// ***INSERT AWAY STATE FUNCTIONS HERE***
+
 //set house state to 'away'
+$state =  array('user' => 'home',
+                        'state' => 2,
+                        'time' => $timestamp );
+                        
+array_push($states, $state); 
 
 }
 
 
-
-
-
-    /**************************************
-    * Play with databases and tables      *
-    **************************************/
+//UPDATE DB
  
     // Prepare INSERT statement to SQLite3 file db
-    $insert = "INSERT INTO states (user, state, time) 
+    $insert = "REPLACE INTO states (user, state, time) 
                 VALUES (:user, :state, :time)";
     $stmt = $file_db->prepare($insert);
  
     // Bind parameters to statement variables
     $stmt->bindParam(':user', $user);
     $stmt->bindParam(':state', $state);
-    $stmt->bindParam(':time', $time);
+    $stmt->bindParam(':time', $timestamp);
  
     // Loop thru all messages and execute prepared insert statement
     foreach ($states as $s) {
       // Set values to bound variables
       $user = $s['user'];
       $state = $s['state'];
-      $time = $s['time'];
+      $timestamp = $s['time'];
  
       // Execute statement
       $stmt->execute();
     }
 
-    /**************************************
-    * Close db connections                *
-    **************************************/
+
+
+/*
+
+//print states for debug
+$sql = 'SELECT * FROM states';
+    foreach ($file_db->query($sql) as $row) {
+        print $row['user'] . "\t";
+        print $row['state'] . "\t";
+        print $row['time'] . "\n";
+    }
+
+
+echo 'm prev state = '.$m_prev_state. "\n";
+echo 'c prev state = '.$c_prev_state;
+
+*/
+
  
     // Close file db connection
     $file_db = null;
